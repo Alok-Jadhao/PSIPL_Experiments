8a3c211fa90539ce5a7e4856ef30e5f8437782f7
Alok Jadhao <144699874+Alok-Jadhao@users.noreply.github.com>
2023-10-31 11:07:36 +0530

    Add files via upload

---
+++ Strings/paragraph.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+//check spaces, new line , characters and words.
+
+int main()
+{
+    char str[100];
+    scanf("%[^~]",str);
+
+    int length = strlen(str)
+
+    for(int i=0;i!="\n";i++)
+    {
+        printf("The number of lines are: ")
+    }
+
+    return 0;
+}
