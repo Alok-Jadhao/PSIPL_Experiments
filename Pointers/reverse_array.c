3730a4e4aff0abb401dda93697cddb2047b8a4c6
Alok Jadhao <144699874+Alok-Jadhao@users.noreply.github.com>
2023-11-10 10:13:08 +0530

    Add files via upload

---
+++ Pointers/reverse_array.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+void reverse_array(int *a,int n)
+{
+    for(int i=0;i<n/2;i++)
+    {
+        int temp;
+        temp = *(a+(n-1-i));
+        *(a+(n-1-i))=*(a+i);
+        *(a+i)= temp;
+
+    }
+}
+
+void print_array(int *a,int n)
+{
+    for(int i=0;i<n;i++)
+        printf("%d  ",*(a+i));
+    printf("\n");
+}
+
+
+
+int main()
+{
+    int a[10],n;
+    printf("Enter no. of elements in array: ");
+    scanf("%d",&n);
+    printf("Enter the array: ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",(a+i));
+    }
+
+    reverse_array(a,n);
+
+    printf("The reversed array is: ");
+    print_array(a,n);
+
+    large_small(a,n);
+
+
+
+    return 0;
+}
